e3df649901dd99b705bb8074e295823df057c937
Gabgame117 <gabin.grigaut@outlook.fr>
2026-03-08 16:08:21 +0100

    pas sur

---
+++ docs/pokémon.js
@@ -0,0 +1,8 @@
+const POKEMONS=[
+
+{name:"Pikachu",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+{name:"Bulbasaur",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"},
+{name:"Charmander",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
+{name:"Squirtle",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"}
+
+]
