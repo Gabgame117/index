4350208380ed608613c9f79f63219b70219211c2
Gabgame117 <gabin.grigaut@outlook.fr>
2026-03-08 19:07:34 +0100

    Delete POKEMONS array from pokémon.js
    Removed the POKEMONS array containing Pokémon data.

--- docs/pokémon.js
+++ docs/pokémon.js
@@ -1,8 +1,1 @@
-const POKEMONS=[
 
-{name:"Pikachu",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-{name:"Bulbasaur",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"},
-{name:"Charmander",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
-{name:"Squirtle",hp:100,sprite:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"}
-
-]
